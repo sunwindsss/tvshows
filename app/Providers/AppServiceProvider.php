42d79a593f24154a63895d899e23e376021bf451
sunwinds <55029946+sunwindsss@users.noreply.github.com>
2024-11-11 02:18:18 +0200

    feat: date/namedays in header
    Added a date and the current name days in the header of the page.
    
    Used Latvian name day calendar data from mixisLv/name-days.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
+use Carbon\Carbon;
+use mixisLv\NameDays\NameDays;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +20,20 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot()
     {
-        //
+        View::composer('layouts.navigation', function ($view) {
+            $carbon = Carbon::now()->locale('lv');
+            $dayName = ucfirst($carbon->translatedFormat('l'));
+            $date = $carbon->format('j');
+            $monthName = $carbon->translatedFormat('F');
+            $fullDate = $dayName . ', ' . $date . '. ' . $monthName;
+
+            // Get Latvian namedays using mixisLv\NameDays\NameDays package
+            $nameDays = new NameDays();
+            $namedays = $nameDays->getNames($carbon->format('m-d'))->toArray();
+
+            $view->with('fullDate', $fullDate)->with('namedays', $namedays);
+        });
     }
 }
